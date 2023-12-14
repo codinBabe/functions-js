9d07b17552bff543a4196fb9fc4ecd6d2394e7e5
codinBabe <teboola2@gmail.com>
2023-12-14 08:32:55 +0100

    first commit

--- question-four.js
+++ question-four.js
@@ -1,6 +1,8 @@
+const word = "This is a text outside a function block";
+
 function displayWords() {
-    return "THis is a text inside a function block"
+    return "THis is a text inside a function block";
 }
+
 console.log(displayWords());
-const word = "This is a text outside a function block"
 console.log(word);
